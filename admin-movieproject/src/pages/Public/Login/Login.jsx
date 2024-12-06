ff69a6481df2f4ea52b72bc950faf69bb4ae1b31
Rose Ann De Guzman <160758582+deguzmanroseann@users.noreply.github.com>
2024-12-07 02:47:05 +0800

    Adding Login, Register, CRUD and Search
    This allows to Login, Register, CRUD and Search Movies from TMDB.

---
+++ admin-movieproject/src/pages/Public/Login/Login.jsx
@@ -0,0 +1,128 @@
+import { useState, useRef, useCallback, useEffect } from 'react';
+import './Login.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isFieldsDirty, setIsFieldsDirty] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [status, setStatus] = useState('idle');
+
+  const navigate = useNavigate();
+
+  const handleShowPassword = useCallback(() => {
+    setIsShowPassword((value) => !value);
+  }, []);
+
+  const handleOnChange = (event, type) => {
+    setIsFieldsDirty(true);
+
+    switch (type) {
+      case 'email':
+        setEmail(event.target.value);
+        break;
+
+      case 'password':
+        setPassword(event.target.value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleLogin = async () => {
+    const data = { email, password };
+    setStatus('loading');
+    console.log(data);
+
+    try {
+      const res = await axios.post('/admin/login', data, {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+      });
+      console.log(res);
+      localStorage.setItem('accessToken', res.data.access_token);
+      navigate('/main/movies');
+      setStatus('idle');
+    } catch (error) {
+      console.error(error);
+      setStatus('idle');
+    }
+  };
+
+  return (
+    <div className="Login">
+      <div className="main-container">
+        <h3>Login</h3>
+        <form>
+          <div className="form-container">
+            <div>
+              <div className="form-group">
+                <label>E-mail:</label>
+                <input
+                  type="text"
+                  name="email"
+                  ref={emailRef}
+                  onChange={(e) => handleOnChange(e, 'email')}
+                />
+              </div>
+              {isFieldsDirty && email === '' && (
+                <span className="errors">This field is required</span>
+              )}
+            </div>
+            <div>
+              <div className="form-group">
+                <label>Password:</label>
+                <input
+                  type={isShowPassword ? 'text' : 'password'}
+                  name="password"
+                  ref={passwordRef}
+                  onChange={(e) => handleOnChange(e, 'password')}
+                />
+              </div>
+              {isFieldsDirty && password === '' && (
+                <span className="errors">This field is required</span>
+              )}
+            </div>
+            <div className="show-password" onClick={handleShowPassword}>
+              {isShowPassword ? 'Hide' : 'Show'} Password
+            </div>
+
+            <div className="submit-container">
+              <button
+                type="button"
+                disabled={status === 'loading'}
+                onClick={() => {
+                  if (email && password) {
+                    handleLogin();
+                  } else {
+                    setIsFieldsDirty(true);
+                    if (email === '') {
+                      emailRef.current.focus();
+                    }
+                    if (password === '') {
+                      passwordRef.current.focus();
+                    }
+                  }
+                }}
+              >
+                {status === 'idle' ? 'Login' : 'Loading'}
+              </button>
+            </div>
+            <div className="register-container">
+              <a href="/register">
+                <small>Register</small>
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
